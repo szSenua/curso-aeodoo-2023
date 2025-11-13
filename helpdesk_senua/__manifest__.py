f6e42b602fca3958d3ae031a1d0087142df5a53c
Senua <chc0089@gmail.com>
2025-11-13 20:41:51 +0000

    Add helpdesk menu and action

--- helpdesk_senua/__manifest__.py
+++ helpdesk_senua/__manifest__.py
@@ -14,6 +14,7 @@
     ],
     "data": [
         "security/helpdesk_security.xml",
-        "security/ir.model.access.csv"
+        "security/ir.model.access.csv",
+        "views/helpdesk_ticket_views.xml"
     ],
 }
